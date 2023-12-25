078b63e40cfd7583e915694d634fbf8966f28722
99Kies <1290017556@qq.com>
2023-12-25 12:24:34 +0800

    update code_id

--- src/mappings/mappingHandlers.ts
+++ src/mappings/mappingHandlers.ts
@@ -176,7 +176,8 @@ export async function handleInstantiateMessage(
   };
   let code_id = msg.msg.decodedMsg["codeId"]["low"];
   // if (code_id === 1) {
-  if (code_id === 5 || code_id === 14) {
+  // if (code_id === 5 || code_id === 14) {
+  if (code_id === 1) {
     logger.info(
       "======================== circuit maci qf !!!!! ========================="
     );
